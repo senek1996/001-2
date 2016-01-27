01f32a7e935b79f86e55ad4a81f99cd42636200a
unknown <bai1541b@k3-426-6.edu.ugrasu>
2016-01-27 17:18:25 +0500

    Вывод еще одной строки

--- src/pkg001/Main.java
+++ src/pkg001/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.print("Hello, World!");
-        // TODO code application logic here
+        System.out.print("\nПривет, мир!");
     }
     
 }
